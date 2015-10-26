ed1c1890a6ce4414b5b84e7fbedafe0d41f39d4f
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2015-10-26 22:49:13 +0800

    changing loading of youtube api to window.onYouTubeIframeAPIReady

--- templates/inner-template/video-playlist.php
+++ templates/inner-template/video-playlist.php
@@ -133,10 +133,12 @@ where (b.rating_scale != NULL OR b.rating_scale != '') AND b.token=%s AND b.doma
 					echo "<script type='text/javascript'>
 							var tag = document.createElement('script');
 							tag.src = 'https://www.youtube.com/iframe_api';
+							
 							var firstScriptTag = document.getElementsByTagName('script')[0];
 							firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+							
 							var player;
-							function onYouTubeIframeAPIReady()
+							window.onYouTubeIframeAPIReady = function()
 							{
 								player = new YT.Player('player', {
 								  height: '400',

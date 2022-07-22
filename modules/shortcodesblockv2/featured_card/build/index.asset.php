92a41ace959cc29f16c564659a27bc19fcbd242f
John Paul Balagolan <johnpaulbalagolan@gmail.com>
2022-07-22 21:50:40 +0800

    Squashed 'wp-content/themes/wp-oese-theme/' changes from 861e17c6..b634e28f
    b634e28f Merge pull request #98 from navnorth/OESE-235
    9825e9b6 OESE-235 set alt text of selected background image to empty when there's no alt text set
    bb626d04 Merge pull request #97 from navnorth/OESE-234
    01609e6e OESE-234 update featured item heading color to #2D8700 and not to show empty headings on Featured Item block
    150cc450 Merge branch 'stage' of https://github.com/navnorth/wp-oese-theme into OESE-234
    e80e7ffa Merge pull request #96 from navnorth/OESE-240
    12fefb42 OESE-240 match line-height of all heading levels on Subpages block
    32d63175 OESE-240 fix mismatch heading display and top margin of different heading levels on edit mode and frontend display
    575aa3f5 OESE-240 fix spacing and font sizes on heading, title and publishing date on Featured Item block
    22bd2d21 OESE-240 fix missing title display on Subpages block
    ec9cb149 OESE-240 fix multiple accordion collapse/expand issue
    438b25ea OESE-240 add publishing date heading tag option on Featured Item block
    6bfa4c97 OESE-240 add Heading and Title tags option on the Featured Item block
    76f465cf OESE-240 add heading select option on accordion block setting
    ca9874f9 OESE-240 add heading tag option to accordion block setting
    aba4c9e4 OESE-240 add heading level option to subpages block setting
    7f51bb56 Merge pull request #95 from navnorth/OESE-235
    6f437bfa OESE-235 set image alt text on featured card block when selecting image via media library
    bd147877 OESE-235 add alt text when selecting image from media library
    ccc30134 Merge pull request #94 from navnorth/OESE-239
    1092fbca OESE-239 adjust z-index of search box and hamburger menu on 200% magnification
    a2a1b726 OESE-239 fix overlapping search box with the page title
    89c23fc2 Merge pull request #93 from navnorth/OESE-237
    cc6cf080 OESE-237 updated the button color to #8D223A on hover/focus of the featured item and card block
    ea94a2d8 Merge pull request #92 from navnorth/OESE-239
    b73b8456 Merge pull request #91 from navnorth/OESE-236
    51250e6d OESE-239 fix search field not clickable on 200% magnification
    3d745a7e Merge branch 'OESE-236' into OESE-239
    5cfd991f OESE-236 change subpages block heading to h3 instead of h4
    c8a71cb5 OESE-236 update accordion block title to h4 instead of h5
    b977a7de Merge pull request #90 from navnorth/OESE-236
    5580f10e OESE-236 update frontend display of featured item block
    ae610487 OESE-236 change heading to h3 and subheading to h4 on featured item block
    c81d1306 OESE-236 change "What can we help you find" heading to h2
    defe7ff3 Merge pull request #89 from navnorth/OESE-239
    982b555b OESE-239 fix display of drop down menus on mobile
    bce00837 OESE-239 adjust hamburger menu position on mobile
    81aa46d2 OESE-239 update style of search box when magnified 200%
    e1fd20bb OESE-239 fix missing search on mobile
    1c897174 OESE-234 update featured item block title color to #2D8700
    608541d2 Merge pull request #88 from navnorth/OESE-237
    7db5fd6d OESE-237 unset blue color on Featured Card block button when focus
    64598fc0 Merge pull request #87 from navnorth/OESE-238
    fbdd09ad OESE-238 add trigger to close icon on keyboard navigation
    0346c182 OESE-238 adjust outline display on focus
    5422a366 OESE-238 fix floating hamburger menu on mobile
    e9e97a32 OESE-238 fix alignment of hamburger menu on mobile
    a9f073a9 OESE-238 add style to hamburger menu on focus
    1c4a13a7 OESE-238 update selector for mobile menu keyboard navigation
    0316ee3d OESE-238 add keyboard navigation on hamburger menu on 200% magnification and on mobile
    735c3981 bump to v2.1.1
    
    git-subtree-dir: wp-content/themes/wp-oese-theme
    git-subtree-split: b634e28f17b4aa33fbe4b8ac13af615149d65ab9

--- modules/shortcodesblockv2/featured_card/build/index.asset.php
+++ modules/shortcodesblockv2/featured_card/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c5a95a07b0e08206c70b5b1e8db2d405');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd4f5cee79cfec5e5691d74ccaed87845');
